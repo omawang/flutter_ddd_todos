52e01757f29dacda2408a2406269889d1b7837fd
Awank <mas.awank100@gmail.com>
2021-01-27 15:28:07 +0700

    notes_overview_body with impl NoteWatcherBloc

--- lib/presentation/notes/notes_overview/notes_overview_page.dart
+++ lib/presentation/notes/notes_overview/notes_overview_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_ddd_todos/application/auth/auth_bloc.dart';
 import 'package:flutter_ddd_todos/application/notes/note_actor/note_actor_bloc.dart';
 import 'package:flutter_ddd_todos/application/notes/note_watcher/note_watcher_bloc.dart';
 import 'package:flutter_ddd_todos/injections.dart';
+import 'package:flutter_ddd_todos/presentation/notes/notes_overview/widgets/notes_overview_body_widget.dart';
 import 'package:flutter_ddd_todos/presentation/routes/router.gr.dart';
 
 class NotesOverviewPage extends StatelessWidget {
@@ -15,7 +16,10 @@ class NotesOverviewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => getIt<NoteWatcherBloc>()),
+        BlocProvider(
+          create: (context) => getIt<NoteWatcherBloc>()
+            ..add(const NoteWatcherEvent.watchAllstarted()),
+        ),
         BlocProvider(create: (context) => getIt<NoteActorBloc>()),
       ],
       child: MultiBlocListener(
@@ -69,9 +73,7 @@ class NotesOverviewPage extends StatelessWidget {
             },
             child: const Icon(Icons.add),
           ),
-          body: const Center(
-            child: Text('a'),
-          ),
+          body: const NotesOverviewBody(),
         ),
       ),
     );
